14c40505723494403a4f732d40bf34701b383d1d
Luis Zapeta <luis.zapeta@plasticosuni.com>
2022-11-30 10:56:56 -0600

    Comentario

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('legacy','anibal','ReactJs100',{
+const db = new Sequelize('legacy','anibal','ReactJs100',{ //Conexion a Base de Datos
     host: "192.168.100.16",
     dialect: "mysql"
 });
